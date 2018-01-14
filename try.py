1ea121107a33b41eb6282fbd7b056b6f44641b6f
Arseny Radushin <radushin.arseny@yandex.ru>
2018-01-14 23:02:18 +0300

    4 try

--- try.py
+++ try.py
@@ -6,4 +6,4 @@ db_query = DbQuery()
 query = """SELECT full_name,auth,id
 	        FROM "user";"""
 query_result=db_query.execute_query(query)
-print(query_result.success)
+print(query_result.value)
