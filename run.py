bbd0e3b5436bf2de67c5c48f9ff84b5f31c9a006
Arseny Radushin <radushin.arseny@yandex.ru>
2018-04-10 04:57:29 +0300

    final version 1.01

--- run.py
+++ run.py
@@ -126,7 +126,7 @@ def dialog(message):
                              "Volgograd, Ekaterinburg, Kazan, Kaliningrad, Moscow, Nizhny Novgorod, Rostov-on-Don, Samara, Saint-Petersburg, Saransk and Sochi \n"
                              "from June 14, 2018 till July 15, 2018\n"
                              "Find detailed schedule <a href='http://www.fifa.com/worldcup/matches/index.html'>here</a>\n\n"
-                             "", parse_mode='HTML')
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/tickets':
             bot.send_message(message.chat.id,
                              "The World Cup tickets are made available in 3 sales phases. Currently, <b>the third sales phase</b> is taking place, which consists of random selection draw period and the first come first served basis.\n\n"
@@ -139,7 +139,8 @@ def dialog(message):
                              + u'\U0001F4CD' + "<a href='https://tickets.fifa.com/FAQ/en?platform=desktop&lang=en'>FAQ</a>\n\n"
                                                "You can also buy ticket-inclusive hospitality packages, each one offering its own unique hospitality experience and service levels though the price will be significantly higher than average.\n"
                              + u'\U0001F4CD' + "Visit the <a href='https://hospitality.fifa.com/hospitality2018/hospitality-packages/emotional-home'>website</a>\n"
-                             + u'\U0001F4CD' + "Find your regional <a href='https://hospitality.fifa.com/hospitality2018/sales-appointed-agents'>sales agents</a>\n",
+                             + u'\U0001F4CD' + "Find your regional <a href='https://hospitality.fifa.com/hospitality2018/sales-appointed-agents'>sales agents</a>\n\n"
+                                               "To return press /menu",
                              parse_mode='HTML')
         elif message.text == '/visa':
             bot.send_message(message.chat.id,
@@ -148,11 +149,13 @@ def dialog(message):
                              "What is FAN ID and how can I get it? - /fan_id \n"
                              "Which details do I need to provide for a FAN ID? - /details \n"
                              "How I can receive FAN ID once it is ready?	- /receive \n"
-                             "FAQ - /faq", parse_mode='HTML')
+                             "FAQ - /faq\n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/free':
             bot.send_message(message.chat.id,
                              "Russia has introduced special entry rules for the <a href='https://en.wikipedia.org/wiki/2018_FIFA_World_Cup'>2018 FIFA World Cup</a> participants and fans. According to these rules, fans are entitled to multiple visa-free entry <b>from 04 June to 25 July 2018</b>.\n"
-                             "You need to obtain the FAN ID after you have bought your tickets or received confirmation for a ticket to a 2018 World Cup match. For the <b>visa-free entry</b> you will need to show your <b>FAN ID</b> and a valid identity document recognized as such by the Russian authorities (e.g. international passport). Please note, that a FAN ID will be also required to leave Russia once it had been used for visa-free entry purposes.",
+                             "You need to obtain the FAN ID after you have bought your tickets or received confirmation for a ticket to a 2018 World Cup match. For the <b>visa-free entry</b> you will need to show your <b>FAN ID</b> and a valid identity document recognized as such by the Russian authorities (e.g. international passport). Please note, that a FAN ID will be also required to leave Russia once it had been used for visa-free entry purposes.\n\n"
+                             "To return press /menu",
                              parse_mode='HTML')
         elif message.text == '/fan_id':
             bot.send_message(message.chat.id,
@@ -162,8 +165,8 @@ def dialog(message):
                              "In order to receive the FAN ID, please fill out and submit your application on www.fan-id.ru or contact one of the FAN ID Distribution centers in any of the host cities.\n\n"
                              "<b>IMPORTANT:</b>\n\n"
                              "During the application process for your FAN ID, you must specify the details from the same identity document that you will use for entry into the Russian Federation."
-                             "If you already have a Russian visa valid within the <a href='https://en.wikipedia.org/wiki/2018_FIFA_World_Cup'>2018 FIFA World Cup</a> period, you still need to apply for a FAN ID in advance to access the stadiums."
-                             , parse_mode='HTML')
+                             "If you already have a Russian visa valid within the <a href='https://en.wikipedia.org/wiki/2018_FIFA_World_Cup'>2018 FIFA World Cup</a> period, you still need to apply for a FAN ID in advance to access the stadiums.\n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/details':
             bot.send_message(message.chat.id,
                              "To fill out the application for a FAN ID, you will need to submit the following data:\n"
@@ -176,7 +179,8 @@ def dialog(message):
                              "- Photo;\n"
                              "- Mobile phone number;\n"
                              "- E-mail address;\n"
-                             "- Mail address for FAN ID delivery.\n")
+                             "- Mail address for FAN ID delivery.\n\n"
+                             "To return press /menu")
         elif message.text == '/receive':
             bot.send_message(message.chat.id,
                              "You can choose postal delivery or delivery through the VFS Global Visa Application centers located outside Russia.\n\n"
@@ -184,7 +188,8 @@ def dialog(message):
                              "The average estimated delivery time for a FAN ID to a foreign country is about 30 days. It usually takes 7 to 11 days for a package to cross the Russian border. Please check the delivery time of your local postal operator and add up these periods in order to estimate your delivery period.\n\n"
                              "<b>2. Delivery through Global Visa Applicant centers</b>\n\n"
                              "The estimated delivery time for a FAN ID to a foreign country is about 30 days.\n\n"
-                             "You can track your FAN ID delivery on the VFS <a href='www.vfsglobal.com'>website</a>",
+                             "You can track your FAN ID delivery on the VFS <a href='www.vfsglobal.com'>website</a>\n\n"
+                             "To return press /menu",
                              parse_mode='HTML')
         elif message.text == '/faq':
             bot.send_message(message.chat.id,
@@ -198,7 +203,8 @@ def dialog(message):
                              u'\U0001F4CD' "<b>I will visit Russia as a tourist and may visit 2018 FIFA World Cup matches</b>\n\n"
                              "You can enter Russia as a tourist in accordance with your visa type.\n\n"
                              "You still need to apply for a FAN ID if you want to access the stadiums. You can visit the one of distribution centers located in the host cities and get a FAN ID while you are in Russia by providing the tickets for the match.\n\n"
-                             "You do not need to apply for a FAN ID if you are not going to access the stadiums.",
+                             "You do not need to apply for a FAN ID if you are not going to access the stadiums.\n\n"
+                             "To return press /menu",
                              parse_mode="HTML")
         elif message.text == '/accommodation':
             bot.send_message(message.chat.id,
@@ -216,12 +222,14 @@ def dialog(message):
                              "If you are staying in a hotel, your hotel is responsible for your registration.\n\n"
                              "<b>Staying in a private apartment:</b>\n\n"
                              "If you are staying in a private house or an apartment, the host will need to register you at the territorial department of the Ministry of Internal Affairs.\n\n"
-                             "<a href='http://welcome2018.com/en/journal/materials/v-gorodakh-kk-2017-izmenen-poryadok-registratsii-po-mestu-prebyvaniya/'>More info</a>",
+                             "<a href='http://welcome2018.com/en/journal/materials/v-gorodakh-kk-2017-izmenen-poryadok-registratsii-po-mestu-prebyvaniya/'>More info</a>\n\n"
+                             "To return press /menu",
                              parse_mode="HTML")
         elif message.text == '/taxi':
             bot.send_message(message.chat.id,
                              "Full list of preferred corporate taxi companies in Russia is <a href='http://moscow.intranet.mckinsey.com/taxi'>here</a> \n\n"
-                             "<b>NOTE:</b> Yandex.Taxi currently available only with Russian sim card.",
+                             "<b>NOTE:</b> Yandex.Taxi currently available only with Russian sim card.\n\n"
+                             "To return press /menu",
                              parse_mode='HTML')
         elif message.text == '/security':
             bot.send_message(message.chat.id,
@@ -240,7 +248,8 @@ def dialog(message):
                              "Volgograd - /volgograd \n"
                              "Sochi - /sochi \n "
                              "Saransk - /saransk \n"
-                             "Rostov-on-Don - /rostov_on_don \n", parse_mode='HTML')
+                             "Rostov-on-Don - /rostov_on_don \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/moscow':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -253,7 +262,8 @@ def dialog(message):
                              "<b>Address</b>: Schepkina street 35, Moscow 129090 \n"
                              "<b>Tel (General)</b>: +7(495) 933 66 55 \n"
                              "<b>Email</b>: cs@emcmos.ru \n"
-                             "<b>Website</b>: http://www.emcmos.ru/en/main \n", parse_mode='HTML')
+                             "<b>Website</b>: http://www.emcmos.ru/en/main \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/saint_petersburg':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -266,7 +276,8 @@ def dialog(message):
                              "<b>Address</b>: 78 Moika emb, Saint Petersburg 190000 \n"
                              "<b>Tel (General)</b>: +7 (812) 740 20 90 \n"
                              "<b>Email</b>: info@amclinic.ru \n"
-                             "<b>Website</b>: http://www.amclinic.com/ \n", parse_mode='HTML')
+                             "<b>Website</b>: http://www.amclinic.com/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/kaliningrad':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -277,14 +288,16 @@ def dialog(message):
                              "<b>Medical center MedExpert</b> \n"
                              "<b>Address</b>: 8, Podpolkovnika Ivannikova street, \n12 F.Leforta boulevard, \n1, Prazhskaya street, Kaliningrad 236040 \n"
                              "<b>Tel (General)</b>: +7(401) 256 77 22 / +7(401) 279 15 15 / +7(401) 256 77 44 \n"
-                             "<b>Website</b>: http://www.med-expert.ru/ \n", parse_mode='HTML')
+                             "<b>Website</b>: http://www.med-expert.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/nizhny_novgorod':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
                              "<b>Privolzhskiy Regional Medical Center</b>\n"
                              "<b>Address</b>: 2 NizhneVolzhskaya embankment, 14 Ilyinskaya street \n 20a Marshala Voronova street, Nizhniy Novgorod 603001 \n"
                              "<b>Tel (General)</b>: +7(831) 428 81 88 \n"
-                             "<b>Website</b>:  http://www.pomc.ru/ \n\n", parse_mode='HTML')
+                             "<b>Website</b>:  http://www.pomc.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/samara':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -295,7 +308,8 @@ def dialog(message):
                              "<b>Samara Regional Clinical Hospital named after Seredavin</b> \n"
                              "<b>Address</b>: 159, Tashkentskaya street, Samara 443095 \n"
                              "<b>Tel (General)</b>: +7 (846) 956 12 60 \n"
-                             "<b>Website</b>: http://www.sokb.ru/ \n", parse_mode='HTML')
+                             "<b>Website</b>: http://www.sokb.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/ekaterinburg':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -308,7 +322,8 @@ def dialog(message):
                              "<b>Address</b>: 113, Sheikmana street, Yekaterinburg 620144 \n"
                              "<b>Tel (General)</b>: +7(343) 283 08 08 \n"
                              "<b>Email</b>: info@ugmk-clinic.ru  \n"
-                             "<b>Website</b>: https://www.ugmk-clinic.ru/ \n", parse_mode='HTML')
+                             "<b>Website</b>: https://www.ugmk-clinic.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/kazan':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -320,7 +335,8 @@ def dialog(message):
                              "<b>Clinic of Family Medicine, OOO</b> \n"
                              "<b>Address</b>: Yamasheva prospekt, 48B, Kazan 420103 \n"
                              "<b>Tel (General)</b>: +7(843) 211 10 11 \n"
-                             "<b>Website</b>: http://www.ksm-kazan.ru/ \n", parse_mode='HTML')
+                             "<b>Website</b>: http://www.ksm-kazan.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/volgograd':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -332,7 +348,8 @@ def dialog(message):
                              "<b>Address</b>: Angarskaya str., 13, Volgograd 400049 \n"
                              "<b>Tel (General)</b>: +7(844) 236 31 66 / +7(844) 236 30 46 \n"
                              "<b>Email</b>: vokb@vokb.net \n"
-                             "<b>Website</b>: https://vokb1.ru/ \n", parse_mode='HTML')
+                             "<b>Website</b>: https://vokb1.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/sochi':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -344,14 +361,16 @@ def dialog(message):
                              "<b>Address</b>: Gagarina street, 19A, Sochi 354000 \n"
                              "<b>Tel (General)</b>: +7(862) 254 55 55 \n"
                              "<b>Email</b>: dms_armed@mail.ru \n"
-                             "<b>Website</b>: http://armed-mc.ru/en/ \n", parse_mode='HTML')
+                             "<b>Website</b>: http://armed-mc.ru/en/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/saransk':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
                              "<b>Republican Clinical Hospital #4</b>\n"
                              "<b>Address</b>: 32, Ulyanova str, Saransk 430032 \n"
                              "<b>Tel (General)</b>: +7(834) 233 42 22 \n"
-                             "<b>Website</b>: http://www.rkb4.ru/ \n\n", parse_mode='HTML')
+                             "<b>Website</b>: http://www.rkb4.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/rostov_on_don':
             bot.send_message(message.chat.id,
                              "<b>Recommended Hospitals</b> \n\n"
@@ -364,7 +383,8 @@ def dialog(message):
                              "<b>Address</b>: 8, Dachnaya street, Rostov on Don 344064 \n"
                              "<b>Tel (General)</b>: +7(863) 223 17 77 / +7(863) 223 24 77 / +7(863) 223 25 75 / +7(863) 223 24 25 / +7(863) 223 24 58 / +7(863) 223 25 77 \n"
                              "<b>Email</b>: info@mc-semya.ru \n"
-                             "<b>Website</b>: http://mc-semya.ru/ \n", parse_mode='HTML')
+                             "<b>Website</b>: http://mc-semya.ru/ \n\n"
+                             "To return press /menu", parse_mode='HTML')
         elif message.text == '/travel':
             bot.send_message(message.chat.id,
                              "If you want to plan sightseeing or entertainment activities, we recommend using the following websites. \n\n"
@@ -378,7 +398,8 @@ def dialog(message):
                                                "To buy tickets to events:\n\n"
                              + u'\U0001F4CD' + "parter.ru/bilety.html?language=en\n\n"
                              + u'\U0001F4CD' + "Kassir.ru \n\n"
-                             + u'\U0001F4CD' + "ticketland.ru", parse_mode='HTML')
+                             + u'\U0001F4CD' + "ticketland.ru\n\n"
+                            "To return press /menu", parse_mode='HTML')
         elif message.text == '/office':
             bot.send_message(message.chat.id,
                              "To mark this momentous event, Moscow office invites you to: \n\n"
@@ -388,7 +409,8 @@ def dialog(message):
                                                "Sign up to participate <a href='https://www.surveymonkey.com/r/V8DH32F'>here</a> \n\n"
                                                "Detailed information will follow. \n\n"
                                                "Stay up to date with the latest news from the Moscow office and get ready for this sensational event! \n\n"
-                                               "If you have questions about our entertainment programs, please send your emails to <b>MSW-2018 FIFA World Cup in Russia.</b>",
+                                               "If you have questions about our entertainment programs, please send your emails to <b>MSW-2018 FIFA World Cup in Russia.</b>\n\n"
+                                               "To return press /menu",
                              parse_mode='HTML')
         else:
             bot.send_message(message.chat.id, "Can't find such command")
